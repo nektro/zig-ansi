411eb48dba466fa5937e410b4265790f9d3be602
Meghan Denny <hello@nektro.net>
2024-05-16 03:24:14 -0700

    this updated

--- build.zig
+++ build.zig
@@ -6,7 +6,7 @@ pub fn build(b: *std.Build) void {
 
     const exe = b.addExecutable(.{
         .name = "zig-ansi",
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = mode,
     });

747d58eb6573d55e5a4986dfb2270811ca256387
Meghan Denny <hello@nektro.net>
2024-05-16 02:49:34 -0700

    those comptimes are no longer needed

--- src/main.zig
+++ src/main.zig
@@ -3,19 +3,19 @@ const std = @import("std");
 const ansi = @import("./lib.zig");
 
 pub fn main() anyerror!void {
-    std.debug.print(comptime ansi.color.Fg(.Red, "All your codebase are belong to us.\n"), .{});
-    std.debug.print(comptime ansi.color.Fg(.Green, "All your codebase are belong to us.\n"), .{});
-    std.debug.print(comptime ansi.color.Fg(.Yellow, "All your codebase are belong to us.\n"), .{});
-    std.debug.print(comptime ansi.color.Fg(.Blue, "All your codebase are belong to us.\n"), .{});
-    std.debug.print(comptime ansi.color.Fg(.Magenta, "All your codebase are belong to us.\n"), .{});
-    std.debug.print(comptime ansi.color.Fg(.Cyan, "All your codebase are belong to us.\n"), .{});
+    std.debug.print(ansi.color.Fg(.Red, "All your codebase are belong to us.\n"), .{});
+    std.debug.print(ansi.color.Fg(.Green, "All your codebase are belong to us.\n"), .{});
+    std.debug.print(ansi.color.Fg(.Yellow, "All your codebase are belong to us.\n"), .{});
+    std.debug.print(ansi.color.Fg(.Blue, "All your codebase are belong to us.\n"), .{});
+    std.debug.print(ansi.color.Fg(.Magenta, "All your codebase are belong to us.\n"), .{});
+    std.debug.print(ansi.color.Fg(.Cyan, "All your codebase are belong to us.\n"), .{});
 
     std.debug.print("\n", .{});
 
-    std.debug.print(comptime ansi.color.Bg(.Red, "All your codebase are belong to us.\n"), .{});
-    std.debug.print(comptime ansi.color.Bg(.Green, "All your codebase are belong to us.\n"), .{});
-    std.debug.print(comptime ansi.color.Bg(.Yellow, "All your codebase are belong to us.\n"), .{});
-    std.debug.print(comptime ansi.color.Bg(.Blue, "All your codebase are belong to us.\n"), .{});
-    std.debug.print(comptime ansi.color.Bg(.Magenta, "All your codebase are belong to us.\n"), .{});
-    std.debug.print(comptime ansi.color.Bg(.Cyan, "All your codebase are belong to us.\n"), .{});
+    std.debug.print(ansi.color.Bg(.Red, "All your codebase are belong to us.\n"), .{});
+    std.debug.print(ansi.color.Bg(.Green, "All your codebase are belong to us.\n"), .{});
+    std.debug.print(ansi.color.Bg(.Yellow, "All your codebase are belong to us.\n"), .{});
+    std.debug.print(ansi.color.Bg(.Blue, "All your codebase are belong to us.\n"), .{});
+    std.debug.print(ansi.color.Bg(.Magenta, "All your codebase are belong to us.\n"), .{});
+    std.debug.print(ansi.color.Bg(.Cyan, "All your codebase are belong to us.\n"), .{});
 }
